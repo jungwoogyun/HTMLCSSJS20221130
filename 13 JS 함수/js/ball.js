76ea4a65a1a15dd226807b9a1f806d4d2261a362
jungwoogyun <84259104+jungwoogyun@users.noreply.github.com>
2022-12-08 15:35:38 +0900

    Add files via upload

---
+++ 13 JS 함수/js/ball.js
@@ -0,0 +1,8 @@
+
+const ballEl=document.querySelector('.box>#ball');
+const start=()=>{
+  ballEl.style.animationPlayState='running';
+}
+const stop=()=>{
+  ballEl.style.animationPlayState='paused';
+}
