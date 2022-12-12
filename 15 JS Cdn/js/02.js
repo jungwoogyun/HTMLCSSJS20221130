fdcaea9b2e7f9c509c1919d495f8097094ae15a8
jungwoogyun <84259104+jungwoogyun@users.noreply.github.com>
2022-12-12 14:04:32 +0900

    Add files via upload

--- 15 JS Cdn/js/02.js
+++ 15 JS Cdn/js/02.js
@@ -1,15 +1,15 @@
-new Swiper('section>.main-banner>.inner>.swiper',{
+new Swiper('.main-banner .swiper',{
   direction:'horizontal', //수평 슬라이드(기본)
   autoplay:{            //시작시 바로 재생 여부
-      delay:3000,
+      delay:1000,
       disableOnInteraction:false //사용자의 action에 따른 슬라이드 동작 정지여부
   },
   loop:true, //무한재생
 
   // navigation 
   navigation:{
-    prevEl:'section>.main-banner>.inner>.swiper>.swiper-button-prev',
-    nextEl:'section>.main-banner>.inner>.swiper>.swiper-button-next'
+    prevEl:'.main-banner .swiper-button-prev',
+    nextEl:'.main-banner .swiper-button-next'
   },
   //pagination
   pagination:{
